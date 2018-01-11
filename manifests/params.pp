7bdd6e3a986924ea022b99af62e03ac4fe743ba3
Miroslav Bauer <mirekys@gmail.com>
2018-01-11 14:59:23 +0100

    Added krb5 defaults to params

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,13 +3,27 @@
 # This class manages shared prameters and variables for the kerberos module
 #
 # [Remember: No empty lines between comments and class definition]
-class kerberos::params {
-
+class kerberos::params (
 # Common values
-  $config_file   = '/etc/krb5.conf'
-  $default_realm = 'LOCAL'
-  $krb4_config   = '/etc/krb.conf'
-  $krb4_realms   = '/etc/krb.realms'
+  $config_file               = '/etc/krb5.conf'
+  $default_realm             = 'LOCAL'
+  $krb4_config               = '/etc/krb.conf'
+  $krb4_realms               = '/etc/krb.realms'
+  $krb5_keytab_realm         = 'LOCAL',
+  $krb5_keytab_service_realm = 'LOCAL',
+  $krb5_keytab_rsh_principal = "host/${::fqdn}@${krb5_keytab_realm}",
+  $krb5_keytab_rsh_host      = 'localhost',
+  $krb5_keytab_services      = ['host'],
+  $krb5_keytab_filename      = '/etc/krb5.keytab',
+  $krb5_keytab_ensure        = present,
+  $krb5_keytab_owner         = 'root',
+  $krb5_keytab_group         = 'root',
+  $krb5_keytab_mode          = '0600',
+  $krb5_keytab_fqdn          = $::fqdn,
+  $krb5_keytab_refresh       = false,
+  $krb5_keytab_no_cache      = false,
+  $krb5_keytab_kerberos      = 'heimdal',
+) {
 
 # OS specific values
   case $::osfamily {
